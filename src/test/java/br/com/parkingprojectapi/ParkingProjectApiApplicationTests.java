a033877d8cc528fe91a2100f0e5953906aca2fad
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-14 17:03:55 -0300

    Project created!

---
+++ src/test/java/br/com/parkingprojectapi/ParkingProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.parkingprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
