db111bd7d14d95a7af7d83890617fbac1869c254
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-15 15:48:42 -0300

    feat: method to insert User created!

--- src/main/java/br/com/parkingprojectapi/service/UserService.java
+++ src/main/java/br/com/parkingprojectapi/service/UserService.java
@@ -1,12 +1,19 @@
 package br.com.parkingprojectapi.service;
 
+import br.com.parkingprojectapi.entity.User;
 import br.com.parkingprojectapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Transactional
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
 }
--- src/main/java/br/com/parkingprojectapi/web/controller/UserController.java
+++ src/main/java/br/com/parkingprojectapi/web/controller/UserController.java
@@ -1,7 +1,11 @@
 package br.com.parkingprojectapi.web.controller;
 
+import br.com.parkingprojectapi.entity.User;
 import br.com.parkingprojectapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        User user = userService.insert(obj);
+        return ResponseEntity.status(201).body(user);
+    }
 }
