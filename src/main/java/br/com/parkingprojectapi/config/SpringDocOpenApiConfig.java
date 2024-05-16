5695e2963ad8105558c6b6b26aa20713016c0edd
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-16 16:37:06 -0300

    docs: Configuring the SpringDoc OpenAPI 3.1 and Swagger 3

---
+++ src/main/java/br/com/parkingprojectapi/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,19 @@
+package br.com.parkingprojectapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().info(new Info().title("REST API - Spring parking")
+                                            .description("API for vehicle parking management")
+                                            .version("v1")
+                                            .contact(new Contact().name("Tulio Lemos").email("tulio@parking.com")));
+    }
+}
