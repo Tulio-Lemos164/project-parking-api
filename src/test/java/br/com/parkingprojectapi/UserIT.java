5e0f2322dc54046b67afd9d7bfb1c27e2af219a5
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-16 18:58:15 -0300

    feat: Testing the User Insert

--- src/test/java/br/com/parkingprojectapi/UserIT.java
+++ src/test/java/br/com/parkingprojectapi/UserIT.java
@@ -1,7 +1,12 @@
 package br.com.parkingprojectapi;
 
+import br.com.parkingprojectapi.web.dto.UserInsertDTO;
+import br.com.parkingprojectapi.web.dto.UserResponseDTO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -12,4 +17,21 @@ public class UserIT {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    public void insertUser_UsernameAndPasswordValid_ReturnCreatedUserStatus201(){
+        UserResponseDTO responseDTO;
+        responseDTO= webTestClient.post()
+                    .uri("/api/v1/users")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(new UserInsertDTO("marshall@gmail.com", "marlil"))
+                    .exchange().expectStatus().isCreated()
+                    .expectBody(UserResponseDTO.class)
+                    .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseDTO).isNotNull();
+        Assertions.assertThat(responseDTO.getId()).isNotNull();
+        Assertions.assertThat(responseDTO.getUsername()).isEqualTo("marshall@gmail.com");
+        Assertions.assertThat(responseDTO.getRole()).isEqualTo("CLIENT");
+    }
 }
