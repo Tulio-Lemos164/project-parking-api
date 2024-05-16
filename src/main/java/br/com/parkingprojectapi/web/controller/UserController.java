6f8db614c430b667af078287a1a27fdb8c113aaa
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-16 16:52:25 -0300

    docs: Documenting the resource to insert a new User

--- src/main/java/br/com/parkingprojectapi/web/controller/UserController.java
+++ src/main/java/br/com/parkingprojectapi/web/controller/UserController.java
@@ -2,10 +2,16 @@ package br.com.parkingprojectapi.web.controller;
 
 import br.com.parkingprojectapi.entity.User;
 import br.com.parkingprojectapi.service.UserService;
+import br.com.parkingprojectapi.web.controller.exceptions.StandardError;
 import br.com.parkingprojectapi.web.dto.UserInsertDTO;
 import br.com.parkingprojectapi.web.dto.UserPasswordDTO;
 import br.com.parkingprojectapi.web.dto.UserResponseDTO;
 import br.com.parkingprojectapi.web.dto.mapper.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Users", description = "Contains all the operations to inset, retrieve and update a user")
 @RestController
 @RequestMapping(value = "api/v1/users")
 public class UserController {
@@ -20,6 +27,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Create a new user on the database",
+                responses = {
+                @ApiResponse(responseCode = "201", description = "User created successfully",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
+                @ApiResponse(responseCode = "409", description = "Username already in use",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class))),
+                @ApiResponse(responseCode = "422", description = "Invalid input data, resources not created",
+                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+    })
     @PostMapping
     public ResponseEntity<UserResponseDTO> insert(@Valid @RequestBody UserInsertDTO obj){
         User user = userService.insert(UserMapper.toUser(obj));
