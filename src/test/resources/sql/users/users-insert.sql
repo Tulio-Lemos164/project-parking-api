c81c65dde7194fe5e878399fcc61f4e079a65781
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-16 18:43:55 -0300

    feat: Configuring the test ambient

---
+++ src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,4 @@
+insert into users (id, username, password, role) values (100, 'ted@gmail.com', 'arqted', 'ROLE_CLIENT');
+insert into users (id, username, password, role) values (200, 'barney@gmail.com', 'barney', 'ROLE_ADMIN');
+insert into users (id, username, password, role) values (150, 'robindcanadian@gmail.com', 'canada', 'ROLE_CLIENT');
+insert into users (id, username, password, role) values (99, 'lily@gmail.com', 'lilmar', 'ROLE_CLIENT');
