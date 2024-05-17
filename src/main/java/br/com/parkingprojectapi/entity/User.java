1bb8e67814a9f76fa24ba547b2792b3e3a6fa1f2
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-17 20:29:52 -0300

    feat: Audit system working!

--- src/main/java/br/com/parkingprojectapi/entity/User.java
+++ src/main/java/br/com/parkingprojectapi/entity/User.java
@@ -6,6 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -14,6 +19,7 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -32,15 +38,20 @@ public class User implements Serializable {
     @Column(name = "role", nullable = false, length = 50)
     private Role role = Role.ROLE_CLIENT;
 
+
+    @CreatedDate
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
 
+    @LastModifiedDate
     @Column(name = "modification_date")
     private LocalDateTime modificationDate;
 
+    @CreatedBy
     @Column(name = "created_by")
     private String createdBy;
 
+    @LastModifiedBy
     @Column(name = "modified_by")
     private String modifiedBy;
 
